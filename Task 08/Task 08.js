33f87c95d0f464843c3abf012742e6f71ef526a9
Mirza Abdullah Baig <abaig7921gmail.com>
2023-07-18 09:31:05 +0500

    Task 08 Done

---
+++ Task 08/Task 08.js
@@ -0,0 +1,12 @@
+// Task 08: Write a program that takes input a number from user & state whether the number is positive, negative or zero.
+
+var number = prompt ("Enter some number");
+
+
+if (number == 0) {
+    document.write("Your Number is Zero");
+} else if (number > 0) {
+    document.write("Your Number is Positive");
+} else {
+    document.write("Your Number is Negative");
+}
